047c588625b5d3fa81add770b0e1f77488daafcd
Helens BuzzLightyearAqua <buzzlightyear0131@163.com>
2022-09-07 15:59:49 +0800

    Test Git

---
+++ src/com/buzz/java_01_Hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.buzz.java_01_Hello;
+
+/**
+ * @author BuzzLightyear
+ * @illustrate:
+ * @data 2022/9/7 15:57
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
